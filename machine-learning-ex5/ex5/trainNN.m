c8a38d86658acdb27d1b72e1d4c2e4c4ab8a434f
Asus <mehhussumon47@gmail.com>
2025-08-28 13:44:48 +0600

    Added fifth assignment

---
+++ machine-learning-ex5/ex5/trainNN.m
@@ -0,0 +1,39 @@
+function [Theta1,Theta2,nn_params,cost] = trainNN(input_layer_size,hidden_layer_size,num_labels,X, y, lambda)
+%TRAINLINEARREG Trains linear regression given a dataset (X, y) and a
+%regularization parameter lambda
+%   [theta] = TRAINLINEARREG (X, y, lambda) trains linear regression using
+%   the dataset (X, y) and regularization parameter lambda. Returns the
+%   trained parameters theta.
+%
+
+% Initialize Theta
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+% Unroll parameters
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+
+fprintf('\nTraining Neural Network... \n')
+
+%  After you have completed the assignment, change the MaxIter to a larger
+%  value to see how more training helps.
+options = optimset('MaxIter', 50);
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+end
